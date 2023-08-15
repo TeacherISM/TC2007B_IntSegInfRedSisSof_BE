d08ea312d765c7a7f74440637f6cff72e33b3777
FranciscoSanchezz <117677307+FranciscoSanchezz@users.noreply.github.com>
2023-08-15 16:29:14 -0600

    correccion

--- A01656793/SEL4C/app1/views.py
+++ A01656793/SEL4C/app1/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from SEL4C.app1.serializers import UserSerializer, GroupSerializer
+from app1.serializers import UserSerializer, GroupSerializer
 
 # Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticateds]
+    permission_classes = [permissions.IsAuthenticated]
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
