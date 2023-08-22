e2ee0f5522753948db942b93652be776847bc1ef
Federico Zenteno <a01660986@tec.mx>
2023-08-21 20:12:38 -0600

    Modelos realizados, binarios eliminados

--- A01660986/SEL4C/app1/serializers.py
+++ A01660986/SEL4C/app1/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from .models import Reminder
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -10,4 +11,10 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+class reminderSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Reminder
+        fields = ['title', 'description', 'date', 'time', 'created_at', 'updated_at']
+
     
--- A01660986/SEL4C/app1/views.py
+++ A01660986/SEL4C/app1/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import UserSerializer, GroupSerializer
+from .models import Reminder
+from .serializers import reminderSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     '''
@@ -18,4 +20,12 @@ class GroupViewSet(viewsets.ModelViewSet):
     '''
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class reminderViewSet(viewsets.ModelViewSet):
+    '''
+    API endpoint that allows reminders to be viewed or edited.
+    '''
+    queryset = Reminder.objects.all()
+    serializer_class = reminderSerializer
     permission_classes = [permissions.IsAuthenticated]
