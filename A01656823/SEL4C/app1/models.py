01673d9702c7cd7ea41ee4dbe77f43941f6db230
Antonio Rafael Cedillo Rodríguez <116749018+AntonioCedilloA01656823@users.noreply.github.com>
2023-08-23 17:37:36 -0600

    limpia

--- A01656823/SEL4C/app1/models.py
+++ A01656823/SEL4C/app1/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class telefonoModel(models.Model):
+    name = models.Charfield(max_length = 100)
+    brand = models.TextField(())
+    sell_date = models.DateTimeField(auto_now = True)
+    
+
+def __str__(self):
+    return self.name
+
+class Meta:
+    app_label = 'app1'
--- A01656823/SEL4C/app1/serializers.py
+++ A01656823/SEL4C/app1/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-
+from .models import telefonoModel
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +12,9 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+        
+
+class TelefonoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = telefonoModel
+        field = {'name','brand','sell_date'}
