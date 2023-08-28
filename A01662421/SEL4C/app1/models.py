a2fb08e5c6eb8674a234d86dceab279e4a553d95
Marco Iván <morkiglogxx@gmail.com>
2023-08-28 17:07:39 -0600

    Custom model y test

--- A01662421/SEL4C/app1/models.py
+++ A01662421/SEL4C/app1/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class HomeModel(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    bedrooms = models.PositiveIntegerField()
+    bathrooms = models.PositiveIntegerField()
+    area = models.DecimalField(max_digits=6, decimal_places=2)
+    address = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    class Meta1:
+        app_label = 'app1'
--- A01662421/SEL4C/app1/views.py
+++ A01662421/SEL4C/app1/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from SEL4C.app1.serializers import UserSerializer, GroupSerializer
-
+from .models import HomeModel
+from .serializers import HomeSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -18,4 +19,12 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class HomeViewSet(viewsets.ModelViewSet):
+    '''
+    API endpoint that allos model to be viewed or edited
+    '''
+    queryset = HomeModel.objects.all()
+    serializer_class = HomeSerializer
     permission_classes = [permissions.IsAuthenticated]
