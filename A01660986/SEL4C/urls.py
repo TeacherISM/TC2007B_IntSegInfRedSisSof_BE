085e7ea0a79105ed017690d9c32b2b285d8b2461
Federico Zenteno <a01660986@tec.mx>
2023-08-28 18:21:59 -0600

    Added Schema urls

--- A01660986/SEL4C/urls.py
+++ A01660986/SEL4C/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from SEL4C.app1 import views
 
 router = routers.DefaultRouter()
@@ -27,4 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
