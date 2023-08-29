a731d26e95327bdea582be2ae03756e82e60ff23
antonioMachorro <a01782114@tec.mx>
2023-08-28 19:16:23 -0600

    Branch con Models y OpenAPI

--- A01782114_virtual/SEL4C/SEL4C/settings.py
+++ A01782114_virtual/SEL4C/SEL4C/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_SCHEMA_CLASS' : 'drf_spectacular.openapi.AutoSchema',
 }
 
 INSTALLED_APPS = [
@@ -43,7 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'app1',
+    #'SEL4C.app1',
 ]
 
 MIDDLEWARE = [
--- A01782114_virtual/SEL4C/SEL4C/urls.py
+++ A01782114_virtual/SEL4C/SEL4C/urls.py
@@ -1,14 +1,19 @@
 from django.urls import include, path
 from rest_framework import routers
 from app1 import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'schools', views.SchoolViewSet)
 
 #Wire up our API using automatic URL lookup.
 #additionally, we include login URLs for browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc')
 ]
