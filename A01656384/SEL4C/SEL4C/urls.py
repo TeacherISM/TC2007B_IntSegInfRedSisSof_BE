c9a31115a3f83e5abee42ee6f12982d574630235
Alfredo-Vqz <a01656384@tec.mx>
2023-08-28 19:24:29 -0700

    cambios

--- A01656384/SEL4C/SEL4C/urls.py
+++ A01656384/SEL4C/SEL4C/urls.py
@@ -1,21 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 from app1 import views
-<<<<<<< Updated upstream
-=======
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 from django.contrib import admin
->>>>>>> Stashed changes
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
-<<<<<<< Updated upstream
-
-urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-=======
 router.register(r'videogames', views.VideogamesViewSet)
 
 urlpatterns = [
@@ -25,5 +16,4 @@ urlpatterns = [
     path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls), 
->>>>>>> Stashed changes
 ]
