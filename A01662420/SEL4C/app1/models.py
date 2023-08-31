8f87d3a66ed10ba31792bb011b30868eeb280636
A01662420 <a01662420@tec.mx>
2023-08-30 19:26:18 -0600

    Clase 3
    Esto fue lo que vimos en la clase 3 con el profesor Iván.

--- A01662420/SEL4C/app1/models.py
+++ A01662420/SEL4C/app1/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
+    class Meta:
+        app_label = 'app1'
+
+
 # Create your models here.
--- A01662420/SEL4C/app1/views.py
+++ A01662420/SEL4C/app1/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 from SEL4C.app1.serializers import UserSerializer, GroupSerializer
+from .models import Producto
+from .serializers import ProductoSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
@@ -12,5 +14,10 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class ProductoViewSet(viewsets.ModelViewSet):
+    queryset=Producto.objects.all()
+    serializer_class = ProductoSerializer
+    permission_classes= [permissions.IsAuthenticated]
 
 # Create your views here.
