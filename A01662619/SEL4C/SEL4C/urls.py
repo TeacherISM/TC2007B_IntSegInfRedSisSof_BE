8e26180ac715623bf473877bfd465a8f697a8dc0
Luis Mtz <116583289+luismtzrmz120@users.noreply.github.com>
2023-08-30 19:43:31 -0600

    Tarea 3
    Esto fue lo que hicimos en la clase 3

--- A01662619/SEL4C/SEL4C/urls.py
+++ A01662619/SEL4C/SEL4C/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from app1 import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'home', views.HomeViewSet)
 
 # Wire op our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -29,4 +31,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
     path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
